d2c9350013ae6a656db2aa3b1682aaa202127599
aalexeev <a.alexeev@fbs.com>
2019-12-10 14:08:51 +0300

    slideshow add

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SlideshowComponent } from './slideshow/slideshow.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SlideshowComponent
   ],
   imports: [
     BrowserModule
